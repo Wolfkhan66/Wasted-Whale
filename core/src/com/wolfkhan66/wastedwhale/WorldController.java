3cf05f51d2db9942805be2fd752b9f9e51e80dd5
Wolfkhan66 <cailehwald@googlemail.com>
2016-07-24 16:38:06 +0100

    Start adding debug controls
    These controls are only active if the app is run on a desktop, the
    selected sprite can now be moved using WASD.
    The wasd events are applied for as long as the keys are pressed and
    provide incremental updates to the sprites translation in whichever
    combination is pressed.

--- core/src/com/wolfkhan66/wastedwhale/WorldController.java
+++ core/src/com/wolfkhan66/wastedwhale/WorldController.java
@@ -1,5 +1,8 @@
 package com.wolfkhan66.wastedwhale;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -77,11 +80,27 @@ public class WorldController {
     }
 
     public void update (float deltaTime) {
+        handleDebugInput(deltaTime);
         updateTestObjects(deltaTime);
     }
 
+    private void handleDebugInput(float deltaTime){
+        if (Gdx.app.getType() != Application.ApplicationType.Desktop) return;
+
+        //Selected Sprite Controls
+        float sprMoveSpeed = 5 * deltaTime;
+        if(Gdx.input.isKeyPressed(Input.Keys.W)) moveSeletedSprite(0, sprMoveSpeed);
+        if(Gdx.input.isKeyPressed(Input.Keys.A)) moveSeletedSprite(-sprMoveSpeed, 0);
+        if(Gdx.input.isKeyPressed(Input.Keys.S)) moveSeletedSprite(0, -sprMoveSpeed);
+        if(Gdx.input.isKeyPressed(Input.Keys.D)) moveSeletedSprite(sprMoveSpeed, 0);
+    }
+    
+    private void moveSeletedSprite(float x, float y){
+        testSprites[selectedSprite].translate(x,y);
+    }
+
     private void updateTestObjects(float deltaTime){
-        // Get current roation from selected sprite
+        // Get current rotation from selected sprite
         float rotation = testSprites[selectedSprite].getRotation();
 
         // Rotate sprite by 90 degrees per second
