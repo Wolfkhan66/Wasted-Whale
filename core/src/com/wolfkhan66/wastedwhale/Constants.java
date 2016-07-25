748af0343a1d255daa56793b896c7a240d330d45
Wolfkhan66 <cailehwald@googlemail.com>
2016-07-25 12:04:18 +0100

    import texturepacker tools to the project, add canyon bunny assets and create a texture atlas of the assets. add an app icon for android.

--- core/src/com/wolfkhan66/wastedwhale/Constants.java
+++ core/src/com/wolfkhan66/wastedwhale/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
 
     // Visible game world is 5 meters tall
     public static final float VIEWPORT_HEIGHT = 5.0f;
+
+
 }
--- desktop/src/com/wolfkhan66/wastedwhale/desktop/DesktopLauncher.java
+++ desktop/src/com/wolfkhan66/wastedwhale/desktop/DesktopLauncher.java
@@ -2,10 +2,26 @@ package com.wolfkhan66.wastedwhale.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.wolfkhan66.wastedwhale.WastedWhaleMain;
 
+
 public class DesktopLauncher {
+
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+
 	public static void main (String[] arg) {
+
+		if (rebuildAtlas){
+			TexturePacker.Settings settings = new TexturePacker.Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "C:/Users/caile_000/Documents/GitHub/Wasted-Whale/desktop/assets-raw/images", "images", "wastedwhale.pack");
+		}
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new WastedWhaleMain(), config);
 		config.width = 800;
