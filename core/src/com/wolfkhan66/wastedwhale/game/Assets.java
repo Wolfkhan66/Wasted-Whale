e7accea48b8c17fee892dce0327750d941d7e77a
Wolfkhan66 <cailehwald@googlemail.com>
2016-07-26 14:21:01 +0100

    Start implementing the GUI
    Added font files for arial 15
    these have been loaded as a BitmapFont.
    The added code includes a new inner class called AssetFonts that holds
    the default bitmap font in three differently configured sizes. Finally
    the Bitmap fonts must be disposed manually so that corresponding calls
    are added to the dispose() method.

--- core/src/com/wolfkhan66/wastedwhale/game/Assets.java
+++ core/src/com/wolfkhan66/wastedwhale/game/Assets.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetErrorListener;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Disposable;
 
@@ -22,9 +23,33 @@ public class Assets implements Disposable, AssetErrorListener{
     public AssetGoldCoin goldCoin;
     public AssetFeather feather;
     public AssetLevelDecoration levelDecoration;
+    public AssetFonts fonts;
 
     // singleton: prevent instantiation from other classes
     private Assets () {}
+
+    public class AssetFonts{
+        public final BitmapFont defaultSmall;
+        public final BitmapFont defaultNormal;
+        public final BitmapFont defaultBig;
+
+        public AssetFonts () {
+            // Create three fonts using LibGDX's 15px bitmap font
+            defaultSmall = new BitmapFont(Gdx.files.internal("images/arial-15.fnt"), true);
+            defaultNormal = new BitmapFont(Gdx.files.internal("images/arial-15.fnt"), true);
+            defaultBig = new BitmapFont(Gdx.files.internal("images/arial-15.fnt"), true);
+
+            // Set font sizes
+            defaultSmall.getData().setScale(0.75f);
+            defaultNormal.getData().setScale(1.0f);
+            defaultBig.getData().setScale(2.0f);
+
+            // Enable linear texture filtering for smooth fonts
+            defaultSmall.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+            defaultNormal.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+            defaultBig.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        }
+    }
 
     public void init (AssetManager assetManager) {
         this.assetManager = assetManager;
@@ -49,6 +74,7 @@ public class Assets implements Disposable, AssetErrorListener{
         }
 
         // Create game resource objects
+        fonts = new AssetFonts();
         bunny = new AssetBunny(atlas);
         rock = new AssetRock(atlas);
         goldCoin = new AssetGoldCoin(atlas);
@@ -59,6 +85,9 @@ public class Assets implements Disposable, AssetErrorListener{
     @Override
     public void dispose() {
         assetManager.dispose();
+        fonts.defaultSmall.dispose();
+        fonts.defaultNormal.dispose();
+        fonts.defaultBig.dispose();
     }
 
     public void error(String filename, Class type, Throwable throwable){
