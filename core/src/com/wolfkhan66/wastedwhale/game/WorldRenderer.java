35aa0956781300fc72fcc7579b2a9692afdfca84
Wolfkhan66 <cailehwald@googlemail.com>
2016-07-26 15:01:16 +0100

    Create a second camera specifically to render the GUI
    The viewport of the GUI camera is defined using a different set of
    constants that uses much higher values, This has to be done correctly to
    render the bitmap font that is 15 pixels high.
    The rest of the code is the same as the game camera and allows for the
    camera to be moved independently from the GUI camera

--- core/src/com/wolfkhan66/wastedwhale/game/WorldRenderer.java
+++ core/src/com/wolfkhan66/wastedwhale/game/WorldRenderer.java
@@ -3,6 +3,7 @@ package com.wolfkhan66.wastedwhale.game;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import com.wolfkhan66.wastedwhale.util.Constants;
 
 /**
  * Created by Cai Lehwald on 24/07/2016.
@@ -11,6 +12,7 @@ public class WorldRenderer implements Disposable {
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private com.wolfkhan66.wastedwhale.game.WorldController worldController;
+    private OrthographicCamera cameraGUI;
 
     public WorldRenderer(com.wolfkhan66.wastedwhale.game.WorldController worldController) {
         this.worldController = worldController;
@@ -21,6 +23,10 @@ public class WorldRenderer implements Disposable {
         camera = new OrthographicCamera(com.wolfkhan66.wastedwhale.util.Constants.VIEWPORT_WIDTH, com.wolfkhan66.wastedwhale.util.Constants.VIEWPORT_HEIGHT);
         camera.position.set(0, 0, 0);
         camera.update();
+        cameraGUI = new OrthographicCamera(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGTH);
+        cameraGUI.position.set(0, 0, 0);
+        cameraGUI.setToOrtho(true); // flip y-axis
+        cameraGUI.update();
     }
 
     public void render() {
@@ -38,6 +44,10 @@ public class WorldRenderer implements Disposable {
     public void resize(int width, int height) {
         camera.viewportWidth = (com.wolfkhan66.wastedwhale.util.Constants.VIEWPORT_WIDTH / height) * width;
         camera.update();
+        cameraGUI.viewportHeight = Constants.VIEWPORT_GUI_HEIGTH;
+        cameraGUI.viewportWidth = Constants.VIEWPORT_GUI_WIDTH / (float)height * (float)width;
+        cameraGUI.position.set(cameraGUI.viewportWidth / 2, cameraGUI.viewportHeight / 2, 0);
+        cameraGUI.update();
     }
 
     @Override public void dispose() {
