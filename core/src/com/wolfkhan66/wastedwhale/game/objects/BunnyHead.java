9bab3baabd1ee75c5848e0e6fec187c2f7ed4037
Wolfkhan66 <cailehwald@googlemail.com>
2016-07-29 15:37:33 +0100

    Start implementing the player init() method
    Initialize the player game object, set up its physics values, give it a
    starting view direction, a jump state and deactivate the feather
    power-up effect.

--- core/src/com/wolfkhan66/wastedwhale/game/objects/BunnyHead.java
+++ core/src/com/wolfkhan66/wastedwhale/game/objects/BunnyHead.java
@@ -1,6 +1,7 @@
 package com.wolfkhan66.wastedwhale.game.objects;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.wolfkhan66.wastedwhale.game.Assets;
 
 /**
  * Created by Cai Lehwald on 29/07/2016.
@@ -27,7 +28,27 @@ public class BunnyHead extends AbstractGameObject {
         init();
     }
 
-    public void init(){};
+    public void init(){
+        dimension.set(1, 1);
+        regHead = Assets.instance.bunny.bunny;
+        // center image on game object
+        origin.set(dimension.x / 2, dimension.y / 2);
+        // Bounding box for collision detection;
+        bounds.set(0, 0, dimension.x, dimension.y);
+        // Set physics values
+        terminalVelocity.set(3.0f, 4.0f);
+        friction.set(12.0f, 0.0f);
+        acceleration.set(0.0f, -25.0f);
+        // View direction
+        view_direction = VIEW_DIRECTION.RIGHT;
+        // Jump state
+        jump_state = JUMP_STATE.FALLING;
+        timeJumping = 0;
+        // Power-ups
+        hasFeatherPowerup = false;
+        timeLeftFeatherPowerup = 0;
+    };
+
     public void setJumping(boolean jumpKeyPressed){};
     public void setFeatherPowerup(boolean pickedUp){};
     public boolean hasFeatherPowerup(){};
